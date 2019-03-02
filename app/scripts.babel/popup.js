7154ad2b47f005272b6c156fff166b9d59a2e6af
thr3a <12jk172@ms.dendai.ac.jp>
2019-03-02 17:36:33 +0900

    update: テーブル表示するようにした

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -4,7 +4,14 @@ chrome.tabs.query({active: true, currentWindow: true}, tabs => {
   const currentTab = tabs[0];
   if(currentTab) {
     chrome.runtime.sendMessage({ action: 'GET_ELMS', tabid: currentTab.id }, response => {
-      console.log(response);
+      const table = document.getElementById('listTable');
+      for (const elm of response.elms) {
+        const row = table.insertRow(-1);
+        const nameCell = row.insertCell(0);
+        const valueCell = row.insertCell(1);
+        nameCell.innerHTML = elm.name;
+        valueCell.innerHTML = elm.value;
+      }
     });
   }
 });
